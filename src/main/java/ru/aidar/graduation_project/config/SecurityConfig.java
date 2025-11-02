832cc69c354e382ba5495418c8c3c887a5f007ae
mathematist <mautaliev0aidar@gmail.com>
2025-11-03 01:01:56 +0300

    Фикс SecurityConfig.java

--- src/main/java/ru/aidar/graduation_project/config/SecurityConfig.java
+++ src/main/java/ru/aidar/graduation_project/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package ru.aidar.graduation_project.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -22,6 +23,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(authz -> authz
+                        .requestMatchers(HttpMethod.GET).permitAll()
                         .requestMatchers("/", "/login", "/default-ui.css").permitAll()
                         .anyRequest().authenticated()
                 )
